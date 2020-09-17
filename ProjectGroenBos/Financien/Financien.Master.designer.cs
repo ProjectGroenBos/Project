fca16b66a442dd01c9dd2b826bb35e1518a9ad70
jeroen avis <jeroenavis123@gmail.com>
2020-09-17 09:51:38 +0200

    Revert

--- ProjectGroenBos/Financien/Financien.Master.designer.cs
+++ ProjectGroenBos/Financien/Financien.Master.designer.cs
@@ -24,13 +24,13 @@ namespace ProjectGroenBos.Financien
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
 
         /// <summary>
-        /// form2 control.
+        /// form1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form2;
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
         /// ContentPlaceHolder1 control.
