034bd0b342761d6931ff999903235aa8593ab7af
René Nieuwenhuizen <629997@student.inholland.nl>
2020-09-20 15:01:24 +0200

    NieuweOO

--- ProjectGroenBos/Schoonmaak en Onderhoud/OnderhoudsOverzicht.aspx
+++ ProjectGroenBos/Schoonmaak en Onderhoud/OnderhoudsOverzicht.aspx
@@ -1,16 +1,33 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="OnderhoudsOverzicht.aspx.cs" Inherits="ProjectGroenBos.Schoonmaak_en_Onderhoud.OnderhoudsOverzicht" %>
+﻿<%@ Page Title="Overzicht Onderhoud" Language="C#" AutoEventWireup="true"  MasterPageFile="~/Site.Master" CodeBehind="OnderhoudsOverzicht.aspx.cs" Inherits="ProjectGroenBos.Schoonmaak_en_Onderhoud.OnderhoudsOverzicht" %>
+<asp:Content ID="OnderhoudsOverzicht" ContentPlaceHolderID="MainContent" runat="server">
 
-<!DOCTYPE html>
 
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-    <title></title>
-</head>
-<body>
-    <form id="form1" runat="server">
-        <div>
-            
-        </div>
-    </form>
-</body>
-</html>
+
+<div class="form-row">
+  <h1 class="text-primary">Overzicht Onderhoud</h1>
+    <div class="row col-sm-12">
+       <div class="col-sm-12">
+          <a style="width:100%; margin-top:1em; margin-bottom:1em" class="btn-primary btn" href="OnderhoudToevoegen.aspx">Klik hier om een nieuwe onderhoud aan te maken</a>
+              <hr />
+          </div>
+
+<div class="col-xs-3">
+  <label for="ex1">Zoek op:</label>                          
+</div>
+
+<div id="divZoek" runat="server" class="col-xs-6">
+     <label ID="txtZoek" runat="server" for="ex3">Voer het onderhoudsnummer in:</label>
+
+     <asp:Label ID="lblErrror" runat="server" ForeColor="Red"></asp:Label>
+     <asp:TextBox ID="searchBox" runat="server" CssClass="form-control" style="width:100%" TextMode="SingleLine" CausesValidation="True"></asp:TextBox>
+</div>
+<div class="col-xs-3">
+   <label for="ex3" style="color:rgba(0, 0, 0, 0)"></label>
+    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:con %>" SelectCommand="SELECT * FROM [Onderhoudsopdracht]"></asp:SqlDataSource>
+    <asp:PlaceHolder ID="PlaceHolder1" runat="server"></asp:PlaceHolder>
+</div>
+
+    </div>
+    </div>
+
+</asp:Content>
--- ProjectGroenBos/Schoonmaak en Onderhoud/OnderhoudsOverzicht.aspx.cs
+++ ProjectGroenBos/Schoonmaak en Onderhoud/OnderhoudsOverzicht.aspx.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,7 +12,47 @@ namespace ProjectGroenBos.Schoonmaak_en_Onderhoud
 {
     public partial class OnderhoudsOverzicht : System.Web.UI.Page
     {
+        StringBuilder table = new StringBuilder(); 
         protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!Page.IsPostBack)
+            {
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = ConfigurationManager.ConnectionStrings["con"].ToString();
+                con.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "select * from [Onderhoudsopdracht]";
+                cmd.Connection = con;
+                SqlDataReader rd = cmd.ExecuteReader();
+                table.Append("<table border='1'>");
+                table.Append("<tr><th>Nummer</th><th>Startdatum</th><th>Verwachtte einddatum</th><th>Kosten</th><th>Bungalownummer</th><th>Onderhoudsstatusnummer</th><th>Onderhoudstypenummer</th><th>Datum van constatering</th>");
+                table.Append("</tr>");
+
+                if(rd.HasRows)
+                {
+                    if (rd.Read())
+                    {
+                        table.Append("<tr>");
+                        table.Append("<td>" + rd[0] + "</td>");
+                        table.Append("<td>" + rd[1] + "</td>");
+                        table.Append("<td>" + rd[2] + "</td>");
+                        table.Append("<td>" + rd[3] + "</td>");
+                        table.Append("<td>" + rd[4] + "</td>");
+                        table.Append("<td>" + rd[5] + "</td>");
+                        table.Append("<td>" + rd[6] + "</td>");
+                        table.Append("<td>" + rd[7] + "</td>");
+                        table.Append("</tr>");
+
+                    }    
+                }
+                
+                table.Append("</table>");
+                PlaceHolder1.Controls.Add(new Literal { Text = table.ToString() });
+                rd.Close();
+            }
+        }
+
+        protected void searchButton_Click(object sender, EventArgs e)
         {
 
         }
