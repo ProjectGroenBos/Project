1bc8cba4ac4c3857398ea055aece1c16015c2087
lucswart <66739939+lucswart@users.noreply.github.com>
2020-09-21 11:11:42 +0200

    EEHH

--- ProjectGroenBos/Recreatie/Helper/Helper.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ProjectGroenBos.Recreatie.Helper
-{
-    public class HelperClass
-    {
-        public static string DatabaseConnectieString = "Data Source=SQL.BIM.OSOX.NL;Initial Catalog=2020-BIM02-P1-P2-Groenbos;Persist Security Info=True;User ID=BIM022020;Password=BiM@IH2020";
-    }
-}
