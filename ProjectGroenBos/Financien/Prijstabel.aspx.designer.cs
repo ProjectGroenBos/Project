a71b58ff08b66e281caadde8db85d2aab80e0053
kevinklkl <64142779+kevinklkl@users.noreply.github.com>
2020-09-27 12:48:50 +0200

    ..

--- ProjectGroenBos/Financien/Prijstabel.aspx.designer.cs
+++ ProjectGroenBos/Financien/Prijstabel.aspx.designer.cs
@@ -7,11 +7,13 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ProjectGroenBos.Financien {
-    
-    
-    public partial class WebForm2 {
-        
+namespace ProjectGroenBos.Financien
+{
+
+
+    public partial class WebForm2
+    {
+
         /// <summary>
         /// gvSeizoenen control.
         /// </summary>
@@ -20,7 +22,25 @@ namespace ProjectGroenBos.Financien {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView gvSeizoenen;
-        
+
+        /// <summary>
+        /// gvFeesdagen control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvFeesdagen;
+
+        /// <summary>
+        /// SqlDataSource4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource4;
+
         /// <summary>
         /// SqlDataSource1 control.
         /// </summary>
@@ -29,7 +49,7 @@ namespace ProjectGroenBos.Financien {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
-        
+
         /// <summary>
         /// gvBungalow control.
         /// </summary>
@@ -38,7 +58,7 @@ namespace ProjectGroenBos.Financien {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView gvBungalow;
-        
+
         /// <summary>
         /// SqlDataSource2 control.
         /// </summary>
@@ -47,7 +67,7 @@ namespace ProjectGroenBos.Financien {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource2;
-        
+
         /// <summary>
         /// gvSeizoentoeslag control.
         /// </summary>
@@ -56,7 +76,7 @@ namespace ProjectGroenBos.Financien {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView gvSeizoentoeslag;
-        
+
         /// <summary>
         /// SqlDataSource3 control.
         /// </summary>
