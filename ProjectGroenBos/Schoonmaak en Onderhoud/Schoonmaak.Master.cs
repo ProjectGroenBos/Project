7103e0594b4fe49e742f53fb93c623ac51958241
jeroen avis <jeroenavis123@gmail.com>
2020-10-18 21:08:29 +0200

    454545

---
+++ ProjectGroenBos/Schoonmaak en Onderhoud/Schoonmaak.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjectGroenBos.Schoonmaak_en_Onderhoud
+{
+    public partial class Schoonmaak : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
