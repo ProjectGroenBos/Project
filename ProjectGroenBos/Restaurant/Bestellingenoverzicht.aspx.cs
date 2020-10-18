b4f0dc7a1363ae455ce048fa330c42e478422cc0
kevss <Kevinpawiro99@gmail.com>
2020-10-18 20:06:48 +0200

    bestelling overzicht
    progressie

--- ProjectGroenBos/Restaurant/Bestellingenoverzicht.aspx.cs
+++ ProjectGroenBos/Restaurant/Bestellingenoverzicht.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,6 +13,44 @@ namespace ProjectGroenBos
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void lnkClickMe_Click(object sender, EventArgs e)
+        {
+            LinkButton b = (LinkButton)sender;
+
+            string arguments = b.CommandArgument;
+            string[] args = arguments.Split(';');
+
+            string Naam = args[0];
+            string ReserveringsNr = args[1];
+            string RondeNr = args[2];
+
+            String mycon = "Data Source=SQL.BIM.OSOX.NL;Initial Catalog=2020-BIM02-P1-P2-Groenbos;Persist Security Info=True;User ID=BIM022020;Password=BiM@IH2020";
+            SqlConnection scon = new SqlConnection(mycon);
+            String myquery = "SELECT ID FROM Item WHERE Naam = '" + Naam + "'";
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = myquery;
+            cmd.Connection = scon;
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+           
+            scon.Open();
+            string ItemID = cmd.ExecuteScalar().ToString();
+            scon.Close();
+            
+            myquery = "UPDATE Item_RestaurantReservering  SET Status = 'Gereed' WHERE ItemID = " + ItemID + " AND RestaurantReserveringID = " + ReserveringsNr + " AND Ronde = " + RondeNr + "";
+
+
+            cmd.CommandText = myquery;
+            scon.Open();
+            cmd.ExecuteNonQuery();
+            scon.Close();
         }
     }
 }
--- ProjectGroenBos/Restaurant/Bestellingenoverzicht.aspx.designer.cs
+++ ProjectGroenBos/Restaurant/Bestellingenoverzicht.aspx.designer.cs
@@ -24,21 +24,12 @@ namespace ProjectGroenBos
         protected global::System.Web.UI.WebControls.GridView GridView1;
 
         /// <summary>
-        /// btnBestellingGereed control.
+        /// SqlDataSource1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnBestellingGereed;
-
-        /// <summary>
-        /// btnWijzigen control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnWijzigen;
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
     }
 }
