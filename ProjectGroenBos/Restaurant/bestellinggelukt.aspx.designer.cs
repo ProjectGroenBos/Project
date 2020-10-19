9ad3b1ff805f50873d6e570963695c4d50781dc0
Lars <71024689+Lorsinator@users.noreply.github.com>
2020-10-19 13:47:37 +0200

    er is nu een bestelling gelukt pagina
    de bestelling gelukt pagina zet het nu alleen nogniet in de database

--- ProjectGroenBos/Restaurant/bestellinggelukt.aspx.designer.cs
+++ ProjectGroenBos/Restaurant/bestellinggelukt.aspx.designer.cs
@@ -33,18 +33,6 @@ namespace ProjectGroenBos.Restaurant
         protected global::System.Web.UI.WebControls.GridView grvBestelling;
 
         /// <summary>
-<<<<<<< HEAD
-=======
-        /// SqlDataSource1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
-
-        /// <summary>
->>>>>>> origin/RestaurantTeam/Kevin
         /// btnTerug control.
         /// </summary>
         /// <remarks>
