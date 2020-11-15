f094acb4b2bc5b4a40ceee4cd1b3bc9248a7e7fd
Daniel655880 <655880@student.inholland.nl>
2020-11-15 20:34:20 +0100

    Klachten formulier werkt

---
+++ ProjectGroenBos/Reservering/KlachtenAfhandeling.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Reservering/Site1.Master" AutoEventWireup="true" CodeBehind="KlachtenAfhandeling.aspx.cs" Inherits="ProjectGroenBos.Reservering.KlachtenAfhandeling" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head2" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+
+    <h1>Klachten pagina</h1>
+
+    <br />
+    <br />
+
+    <asp:RadioButton ID="rdbBezoeker" runat="server" Text="Geen bestaande gast" groupname="rdbGastCheck" CssClass="labelsnieuw" AutoPostBack="True"></asp:RadioButton>
+    <br />
+    <asp:RadioButton ID="rdbGast" runat="server" Text="Bestaande gast" groupname="rdbGastCheck" CssClass="labelsnieuw" Checked="True" AutoPostBack="True"></asp:RadioButton>
+
+    <br />
+    <br />
+
+    <asp:Label CssClass="labelsnieuw" runat="server" Text="Gastnummer"></asp:Label>
+
+    <asp:TextBox ID="txbGastnummer"  CssClass="textboxchaos" runat="server"></asp:TextBox>
+
+    <br />
+    <br />
+    
+    <asp:Label CssClass="labelsnieuw" runat="server" Text="Voornaam"></asp:Label>
+
+    <asp:TextBox ID="txbVoornaam"  CssClass="textboxchaos" runat="server"></asp:TextBox>
+
+    <br />
+    <br />
+
+    <asp:Label CssClass="labelsnieuw" runat="server" Text="Achternaam"></asp:Label>
+
+    <asp:TextBox ID="txbAchernaam"  CssClass="textboxchaos" runat="server"></asp:TextBox>
+
+    <br />
+    <br />
+
+    <asp:Label CssClass="labelsnieuw" runat="server" Text="Klacht"></asp:Label>
+
+    <asp:TextBox ID="txbKlacht" CssClass="textboxchaos" runat="server" TextMode="MultiLine" Height="200px" Width="300px"></asp:TextBox>
+
+    <br />
+    <br />
+
+    <asp:Button ID="btnVerzenden" CssClass="textboxbtn" runat="server" Text="Verzenden" OnClick="btnVerzenden_Click" />
+
+</asp:Content>
---
+++ ProjectGroenBos/Reservering/KlachtenAfhandeling.aspx.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjectGroenBos.Reservering
+{
+    public partial class KlachtenAfhandeling : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (rdbBezoeker.Checked == true)
+            {
+
+                txbVoornaam.Visible = true;
+                txbAchernaam.Visible = true;
+                txbGastnummer.Visible = false;
+                txbGastnummer.Text = "";
+            }
+            else if (rdbGast.Checked == true)
+            {
+                txbVoornaam.Visible = false;
+                txbAchernaam.Visible = false;
+                txbGastnummer.Visible = true;
+                txbVoornaam.Text = "";
+                txbAchernaam.Text = "";
+            }
+            else
+            {
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Er moet een radiobutton geselecteerd zijn.')", true);
+            }
+
+            
+
+        }
+
+        protected void btnVerzenden_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["2020-BIM02-P1-P2-GroenbosConnectionString"].ConnectionString))
+            {
+                string voornaam = txbVoornaam.Text;
+                string achternaam = txbAchernaam.Text;
+                string klacht = txbKlacht.Text;
+
+                if(rdbBezoeker.Checked == true)
+                {
+
+                    con.Open();
+
+
+                    string query = "insert into Klacht (Omschrijving, Voornaam, Achternaam)values(@klachtomschrijving, @voornaam, @achternaam)";
+
+                    SqlCommand cmd = new SqlCommand(query, con);
+
+                    cmd.Parameters.AddWithValue("@klachtomschrijving", klacht);
+                    cmd.Parameters.AddWithValue("@voornaam", voornaam);
+                    cmd.Parameters.AddWithValue("@achternaam", achternaam);
+
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.ExecuteNonQuery();
+
+                    con.Close();
+
+                    Response.Redirect("Huisjemedewerker.aspx");
+
+                }
+                else
+                {
+
+                    int gastnummer = int.Parse(txbGastnummer.Text);
+                    
+                    con.Open();
+
+
+                    string query = "insert into Klacht (Omschrijving, GastNummer, Voornaam, Achternaam)values(@klachtomschrijving, @gastnummer, @voornaam, @achternaam)";
+
+                    SqlCommand cmd = new SqlCommand(query, con);
+
+                    cmd.Parameters.AddWithValue("@klachtomschrijving", klacht);
+                    cmd.Parameters.AddWithValue("@gastnummer", gastnummer);
+                    cmd.Parameters.AddWithValue("@voornaam", voornaam);
+                    cmd.Parameters.AddWithValue("@achternaam", achternaam);
+
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.ExecuteNonQuery();
+
+                    con.Close();
+
+                    Response.Redirect("Huisjemedewerker.aspx");
+                }
+
+            }
+        }
+    }
+}
