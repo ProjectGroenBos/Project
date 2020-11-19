540911f082f649fdd9a50f20ceb542e18a9a8859
lucswart <66739939+lucswart@users.noreply.github.com>
2020-11-19 10:12:36 +0100

    Revert "Merge remote-tracking branch 'origin/SchoonmaakTeam/Rene2' into Financiën"
    This reverts commit e7405cc8c4ac1b8504703c01dd00b8ce69e6269a, reversing
    changes made to fe74eca68293e1f04da855d7f89f430c5035ae3d.

--- ProjectGroenBos/Schoonmaak en Onderhoud/OnderhoudsOverzicht.aspx.cs
+++ ProjectGroenBos/Schoonmaak en Onderhoud/OnderhoudsOverzicht.aspx.cs
@@ -19,7 +19,60 @@ namespace ProjectGroenBos.Schoonmaak_en_Onderhoud
         StringBuilder table = new StringBuilder();
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            //if (!this.IsPostBack)
+            //{
+            ////Populating a DataTable from database.
+            //DataTable dt = this.GetData();
+
+
+
+            ////Building an HTML string.
+            //StringBuilder html = new StringBuilder();
+
+
+
+            ////Table start.
+            //html.Append("<table border = '1'>");
+
+
+
+            ////Building the Header row.
+            //html.Append("<tr>");
+            ////foreach (DataColumn column in dt.Columns)
+            ////{
+            ////html.Append("<th>");
+            ////html.Append(column.ColumnName);
+            //html.Append("<th>Nummer</th><th>Startdatum</th><th>[Verwachte einddatum]</th><th>Kosten</th><th>Bungalownummer</th><th>Onderhoudsstatusnummer</th><th>Onderhoudstypenummer</th><th>[Datum van constatering]</th>");
+
+            ////html.Append("</th>");
+            ////}
+            //html.Append("</tr>");
+
+
+
+            ////Building the Data rows.
+            //foreach (DataRow row in dt.Rows)
+            //{
+            //    html.Append("<tr>");
+            //    foreach (DataColumn column in dt.Columns)
+            //    {
+            //        html.Append("<td>");
+            //        html.Append(row[column.ColumnName]);
+            //        html.Append("</td>");
+            //    }
+            //    html.Append("</tr>");
+            //}
+
+
+
+            ////Table end.
+            //html.Append("</table>");
+
+
+
+            ////Append the HTML string to Placeholder.
+            //PlaceHolder1.Controls.Add(new Literal { Text = html.ToString() });
+
         }
 
         protected void btnZoek_Click(object sender, EventArgs e)
@@ -79,4 +132,134 @@ namespace ProjectGroenBos.Schoonmaak_en_Onderhoud
             Response.Redirect("OnderhoudWijzigen.aspx");
         }
     }
+
+
+
+    //private DataTable GetData()
+    //{
+    //    string constr = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+    //    using (SqlConnection con = new SqlConnection(constr))
+    //    {
+    //        using (SqlCommand cmd = new SqlCommand("SELECT Nummer, Startdatum, [VerwachteEinddatum], Kosten, BungalowNummer, OnderhoudsstatusID, OnderhoudtypeID, [DatumvanConstatering] FROM Onderhoudsopdracht"))
+    //        {
+    //            using (SqlDataAdapter sda = new SqlDataAdapter())
+    //            {
+    //                cmd.Connection = con;
+    //                sda.SelectCommand = cmd;
+    //                using (DataTable dt = new DataTable())
+    //                {
+    //                    sda.Fill(dt);
+    //                    return dt;
+    //                }
+    //            }
+    //        }
+    //    }
+    //}
+
+
+
+    //private DataTable GetData1()
+    //{
+    //    string constr = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+    //    using (SqlConnection con = new SqlConnection(constr))
+    //    {
+    //        using (SqlCommand cmd = new SqlCommand("select Nummer, Startdatum, [VerwachteEinddatum], Kosten, BungalowNummer, OnderhoudsstatusID, OnderhoudtypeID, [DatumvanConstatering] from Onderhoudsopdracht where Nummer= '" + searchBox.Text + "'"))
+    //        {
+    //            using (SqlDataAdapter sda = new SqlDataAdapter())
+    //            {
+    //                cmd.Connection = con;
+    //                sda.SelectCommand = cmd;
+    //                using (DataTable dt = new DataTable())
+    //                {
+    //                    sda.Fill(dt);
+    //                    return dt;
+    //                }
+    //            }
+    //        }
+    //    }
+    //}
+
+
+
+
+    //protected void searchButton_Click(object sender, EventArgs e)
+    //    {
+
+    //    }
+
+
+
+    //    protected void searchBox_TextChanged(object sender, EventArgs e)
+    //    {
+
+    //    }
+
+
+
+    //    protected void Button1_Click(object sender, EventArgs e)
+    //    {
+    ////"select * from [dbo].[Onderhoudsopdracht] where Nummer= '" + searchBox.Text + "'"
+
+
+    ////Populating a DataTable from database.
+    //DataTable dt = this.GetData1();
+
+
+
+    ////Building an HTML string.
+    //StringBuilder html = new StringBuilder();
+
+
+
+    ////Table start.
+    //html.Append("<table border = '1'>");
+
+
+
+    ////Building the Header row.
+    //html.Append("<tr>");
+    ////foreach (DataColumn column in dt.Columns)
+    ////{
+    ////html.Append("<th>");
+    ////html.Append(column.ColumnName);
+    //html.Append("<th>Nummer</th><th>Startdatum</th><th>Verwachte einddatum</th><th>Kosten</th><th>Bungalownummer</th><th>Onderhoudsstatusnummer</th><th>Onderhoudstypenummer</th><th>Datum van constatering</th>");
+
+
+
+    ////html.Append("</th>");
+    ////}
+    //html.Append("</tr>");
+
+
+
+    //Building the Data rows.
+    //    foreach (DataRow row in dt.Rows)
+    //    {
+    //        html.Append("<tr>");
+    //        foreach (DataColumn column in dt.Columns)
+    //        {
+    //            html.Append("<td>");
+    //            html.Append(row[column.ColumnName]);
+    //            html.Append("</td>");
+    //        }
+    //        html.Append("</tr>");
+    //    }
+
+
+
+    //    //Table end.
+    //    html.Append("</table>");
+
+
+
+    //    //Append the HTML string to Placeholder.
+    //    PlaceHolder1.Controls.Add(new Literal { Text = html.ToString() });
+
+    //}
+
+    //protected void Button2_Click(object sender, EventArgs e)
+    //    {
+    //        Response.Redirect("OnderhoudToevoegen.aspx");
+    //    }
+    //}
 }
--- ProjectGroenBos/Schoonmaak en Onderhoud/SchouwingsOverzicht.aspx.cs
+++ ProjectGroenBos/Schoonmaak en Onderhoud/SchouwingsOverzicht.aspx.cs
@@ -11,75 +11,7 @@ namespace ProjectGroenBos.Schoonmaak_en_Onderhoud
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            GridView1.DataBind();
-            txtDatum.Visible = false;
-            drop2.Visible = false;
-        }
 
-        protected void btnToevoegen_Click(object sender, EventArgs e)
-        {
-            Response.Redirect("SchouwingToevoegen.aspx");
-        }
-
-        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            Session["SchouwingID"] = (string)GridView1.SelectedRow.Cells[1].Text;
-            Session["BungalowID"] = (string)GridView1.SelectedRow.Cells[2].Text;
-            Session["Schouwingsdatum"] = (string)GridView1.SelectedRow.Cells[3].Text;
-            Session["Omschrijving"] = (string)GridView1.SelectedRow.Cells[4].Text;
-
-            Response.Redirect("SchouwingWijzigen.aspx");
-        }
-
-        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if (DropDownList1.SelectedItem.Value == "Bungalownummer")
-            {
-                txtDatum.Visible = false;
-                drop2.Visible = true;
-            }
-            if (DropDownList1.SelectedItem.Value == "Schouwingsdatum")
-            {
-                txtDatum.Visible = true;
-                drop2.Visible = false;
-            }
-        }
-
-        protected void Button1_Click(object sender, EventArgs e)
-        {
-            if (DropDownList1.SelectedItem.Value == "Bungalownummer")
-            {
-                GridView1.Visible = false;
-                gridDatum.Visible = false;
-                drop2.Visible = true;
-
-            }
-            if (DropDownList1.SelectedItem.Value == "Schouwingsdatum")
-            {
-                GridView1.Visible = false;
-                gridBungalow.Visible = false;
-                txtDatum.Visible = true;
-            }
-        }
-
-        protected void gridBungalow_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            Session["SchouwingID"] = (string)gridBungalow.SelectedRow.Cells[1].Text;
-            Session["BungalowID"] = (string)gridBungalow.SelectedRow.Cells[2].Text;
-            Session["Schouwingsdatum"] = (string)gridBungalow.SelectedRow.Cells[3].Text;
-            Session["Omschrijving"] = (string)gridBungalow.SelectedRow.Cells[4].Text;
-
-            Response.Redirect("SchouwingWijzigen.aspx");
-        }
-
-        protected void gridDatum_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            Session["SchouwingID"] = (string)gridDatum.SelectedRow.Cells[1].Text;
-            Session["BungalowID"] = (string)gridDatum.SelectedRow.Cells[2].Text;
-            Session["Schouwingsdatum"] = (string)gridDatum.SelectedRow.Cells[3].Text;
-            Session["Omschrijving"] = (string)gridDatum.SelectedRow.Cells[4].Text;
-
-            Response.Redirect("SchouwingWijzigen.aspx");
         }
     }
 }
