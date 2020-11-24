28f4e62433e08df348ae554a97fcabdb85e5bbe7
Daan Vlaar <644013@student.inholland.nl>
2020-11-24 16:20:22 +0100

    Heyyyyyyyyyyyyy

---
+++ ProjectGroenBos/Restaurant/Voorraad.aspx
@@ -0,0 +1,121 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Restaurant/Restaurant.Master" AutoEventWireup="true" CodeBehind="Voorraad.aspx.cs" Inherits="ProjectGroenBos.Restaurant.Voorraad" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Voorraad overzicht</h1>
+    <div>
+            Zoeken:<asp:TextBox ID="txbZoeken" runat="server" OnTextChanged="txbZoeken_TextChanged" Width="1169px" AutoPostBack="True"></asp:TextBox>
+            <asp:GridView ID="gvBekijkenVoorraad" runat="server" AutoGenerateColumns="False" ShowFooter ="true" DataKeyNames="ID" ShowHeaderWhenEmpty="true"
+
+              onrowcommand="gvBekijkenVoorraad_rowcommand" onrowediting ="gvBekijkenVoorraad_rowediting" onrowcancelingedit="gvBekijkenVoorraad_rowcancelingedit" onrowupdating ="gvBekijkenVoorraad_rowupdating" onrowdeleting ="gvBekijkenVoorraad_rowdeleting"
+            
+            cellpadding="4" forecolor="#333333" gridlines="none" width="190px" onselectedindexchanged="gvBekijkenVoorraad_selectedindexchanged" style="height: 100%; width: 90%">
+            
+            <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+            <EditRowStyle BackColor="#999999" />
+            <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+            <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#E9E7E2" />
+            <SortedAscendingHeaderStyle BackColor="#506C8C" />
+            <SortedDescendingCellStyle BackColor="#FFFDF8" />
+            <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+
+
+<Columns>
+
+                        <asp:TemplateField HeaderText="ProductID">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("ID") %>' runat="server" />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtProductID" Text='<%# Eval("ID") %>' runat="server" />
+                        </EditItemTemplate>
+                    </asp:TemplateField>
+
+                    <asp:TemplateField HeaderText="Producten op Voorraad">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("Aantal") %>' runat="server" />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtAantalVoorraad" Text='<%# Eval("Aantal") %>' runat="server" />
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtAantalVoorraadFooter" runat="server" />
+                        </FooterTemplate>
+                    </asp:TemplateField>
+                    
+                    <asp:TemplateField HeaderText="Productnaam">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("Naam") %>' runat="server" />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtProductNaam" Text='<%# Eval("Naam") %>' runat="server" />
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtProductnaamFooter" runat="server" />
+                        </FooterTemplate>
+                    </asp:TemplateField>
+
+                    <asp:TemplateField HeaderText="Inkoopprijs">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("Prijs") %>' runat="server" />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtPrijs" Text='<%# Eval("Prijs") %>' runat="server" />
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtPrijsFooter" runat="server" />
+                        </FooterTemplate>
+                    </asp:TemplateField>
+
+                    <asp:TemplateField HeaderText="Minimum Voorraad">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("Minimum voorraad") %>' runat="server" />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtMinimumVoorraad" Text='<%# Eval("Minimum voorraad") %>' runat="server" />
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtMinimumVoorraadFooter" runat="server" />
+                        </FooterTemplate>
+                    </asp:TemplateField>
+
+                        <asp:TemplateField HeaderText="BTW Tarief">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("BTW tariefID") %>' runat="server" />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtBTWTarief" Text='<%# Eval("BTW tariefID") %>' runat="server" />
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtBTWTariefFooter" runat="server" />
+                        </FooterTemplate>
+                    </asp:TemplateField>
+
+                    <asp:TemplateField>
+                        <ItemTemplate>
+                            <asp:ImageButton ImageUrl="~/Restaurant/img/edit.png" runat="server" CommandName="Edit" ToolTip="Bewerken" Width="20px" Height="20px" />
+                            <asp:ImageButton ImageUrl="~/Restaurant/img/delete.png" runat="server" CommandName="Delete" ToolTip="Verwijderen" Width="20px" Height="20px" />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:ImageButton ImageUrl="~/Restaurant/img/save.png" runat="server" CommandName="Update" ToolTip="Opslaan" Width="20px" Height="20px" ValidationGroup="Validation" />
+                            <asp:ImageButton ImageUrl="~/Restaurant/img/cancel.png" runat="server" CommandName="Cancel" ToolTip="Annuleren" Width="20px" Height="20px" />
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:ImageButton ImageUrl="~/Restaurant/img/addnew.png" runat="server" CommandName="AddNew" ToolTip="Toevoegen" Width="20px" Height="20px" ValidationGroup="FooterValidation" />
+                        </FooterTemplate>
+                    </asp:TemplateField>
+              </Columns>
+        </asp:GridView>
+
+       
+        <br />
+        <asp:Label ID ="lblSuccessMessage" Text="" runat="server" ForeColor="Green" />
+         <br />
+
+        </div>
+        <asp:Label ID ="lblErrorMessage" Text="" runat="server" ForeColor="Red" />
+</asp:Content>
---
+++ ProjectGroenBos/Restaurant/Voorraad.aspx.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace ProjectGroenBos.Restaurant
+{
+    public partial class Voorraad : System.Web.UI.Page
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["dbconnectie"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                InvullenGridview();
+            }
+        }
+
+        protected void btnBevestig_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView1_SelectedIndexChanged1(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void gvBekijkenVoorraad_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        void InvullenGridview()
+        {
+            DataTable dtbl = new DataTable();
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("select * from Voorraad where Omschrijving='Frisdranken' or Omschrijving='Voedsel';", sqlCon);
+                sqlDa.Fill(dtbl);
+                Session["vLE"] = dtbl;
+            }
+            if (dtbl.Rows.Count > 0)
+            {
+                gvBekijkenVoorraad.DataSource = dtbl; //!
+                gvBekijkenVoorraad.DataBind();
+            }
+
+            else
+            {
+                dtbl.Rows.Add(dtbl.NewRow());
+                gvBekijkenVoorraad.DataSource = dtbl;
+                gvBekijkenVoorraad.DataBind();
+                gvBekijkenVoorraad.Rows[0].Cells.Clear();
+                gvBekijkenVoorraad.Rows[0].Cells.Add(new TableCell());
+                gvBekijkenVoorraad.Rows[0].Cells[0].ColumnSpan = dtbl.Columns.Count;
+                gvBekijkenVoorraad.Rows[0].Cells[0].Text = "Geen Data Gevonden!";
+                gvBekijkenVoorraad.Rows[0].Cells[0].HorizontalAlign = HorizontalAlign.Center;
+            }
+
+        }
+
+        protected void txbZoeken_TextChanged(object sender, EventArgs e)
+        {
+            DataTable dtbl = (DataTable)Session["vLE"];
+            DataView dv = dtbl.DefaultView;
+            dv.RowFilter = string.Format("naam like '%{0}%'", txbZoeken.Text);
+            gvBekijkenVoorraad.DataSource = dv.ToTable();
+            gvBekijkenVoorraad.DataBind();
+        }
+
+        protected void gvBekijkenVoorraad_selectedindexchanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void gvBekijkenVoorraad_rowcommand(object sender, GridViewCommandEventArgs e)
+        {
+            try
+            {
+                if (e.CommandName.Equals("AddNew"))
+                {
+                    using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                    {
+                        sqlCon.Open();
+                        String query = "INSERT INTO Voorraad (Aantal, Naam, Prijs, [Minimum voorraad], [BTW tariefID], Omschrijving) VALUES (@aantal,@naam,@prijs,@minimumvoorraad,@btwtarief,@omschrijving)";
+                        SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                        sqlCmd.Parameters.AddWithValue("@aantal", (gvBekijkenVoorraad.FooterRow.FindControl("txtAantalVoorraadFooter") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@naam", (gvBekijkenVoorraad.FooterRow.FindControl("txtProductNaamFooter") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@prijs", (gvBekijkenVoorraad.FooterRow.FindControl("txtPrijsFooter") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@minimumvoorraad", (gvBekijkenVoorraad.FooterRow.FindControl("txtMinimumVoorraadFooter") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@btwtarief", (gvBekijkenVoorraad.FooterRow.FindControl("txtBTWTariefFooter") as TextBox).Text.Trim());
+                        SqlParameter sqlParameter = sqlCmd.Parameters.AddWithValue("@omschrijving", "Voedsel".Trim());
+                        sqlCmd.ExecuteNonQuery();
+                        InvullenGridview();
+                        lblSuccessMessage.Text = "Product toegevoegd aan voorraad";
+                        lblErrorMessage.Text = "";
+                    }
+                }
+        }
+            catch (Exception ex)
+            {
+                lblSuccessMessage.Text = "";
+                lblErrorMessage.Text = ex.Message;
+                throw;
+            }
+}
+
+        protected void gvBekijkenVoorraad_rowediting(object sender, GridViewEditEventArgs e)
+        {
+            gvBekijkenVoorraad.EditIndex = e.NewEditIndex;
+            InvullenGridview();
+        }
+
+        protected void gvBekijkenVoorraad_rowcancelingedit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gvBekijkenVoorraad.EditIndex = -1;
+            InvullenGridview();
+        }
+
+        protected void gvBekijkenVoorraad_rowdeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            try
+            {
+                using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                {
+                    sqlCon.Open();
+                    String query = "DELETE FROM Voorraad WHERE ID = @id";
+                    SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                    sqlCmd.Parameters.AddWithValue("@id", Convert.ToInt32(gvBekijkenVoorraad.DataKeys[e.RowIndex].Value.ToString()));
+                    sqlCmd.ExecuteNonQuery();
+                    InvullenGridview();
+                    lblSuccessMessage.Text = "Gekozen rij verwijderd";
+                    lblErrorMessage.Text = "";
+                }
+            }
+
+            catch (Exception ex)
+            {
+                lblSuccessMessage.Text = "";
+                lblErrorMessage.Text = ex.Message;
+                throw;
+            }
+        }
+
+        protected void gvBekijkenVoorraad_rowupdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                {
+                    sqlCon.Open();
+                    String query = "UPDATE Voorraad SET Aantal = @aantal, Naam = @naam, Prijs = @prijs, [Minimum voorraad] = @minimumvoorraad, [BTW tariefID] = @BTWTarief WHERE ID = @id";
+                    SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                    sqlCmd.Parameters.AddWithValue("@aantal", (gvBekijkenVoorraad.Rows[e.RowIndex].FindControl("txtAantalVoorraad") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@naam", (gvBekijkenVoorraad.Rows[e.RowIndex].FindControl("txtProductNaam") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@prijs", (gvBekijkenVoorraad.Rows[e.RowIndex].FindControl("txtPrijs") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@minimumvoorraad", (gvBekijkenVoorraad.Rows[e.RowIndex].FindControl("txtMinimumVoorraad") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@BTWTarief", (gvBekijkenVoorraad.Rows[e.RowIndex].FindControl("txtBTWTarief") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@id", Convert.ToInt32(gvBekijkenVoorraad.DataKeys[e.RowIndex].Value.ToString()));
+                    sqlCmd.ExecuteNonQuery();
+                    gvBekijkenVoorraad.EditIndex = -1;
+                    InvullenGridview();
+                    lblSuccessMessage.Text = "Product gewijzigd";
+                    lblErrorMessage.Text = "";
+                }
+        }
+            catch (Exception ex)
+            {
+                lblSuccessMessage.Text = "";
+                lblErrorMessage.Text = ex.Message;
+                throw;
+            }
+}
+    }
+}
