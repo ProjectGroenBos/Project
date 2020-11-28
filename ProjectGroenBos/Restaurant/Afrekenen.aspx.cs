50227301765cf4fd7ee17d55df8110a7cb54475b
Lars <71024689+Lorsinator@users.noreply.github.com>
2020-11-28 12:59:36 +0100

    54745

--- ProjectGroenBos/Restaurant/Afrekenen.aspx.cs
+++ ProjectGroenBos/Restaurant/Afrekenen.aspx.cs
@@ -30,17 +30,29 @@ namespace ProjectGroenBos.Restaurant
 
 		protected void btnRekening_Click(object sender, EventArgs e)
 		{
-
+			// Verbinding met de database maken en een factuur aanmaken voor op rekening
+			String mycon = "Data Source=SQL.BIM.OSOX.NL;Initial Catalog=2020-BIM02-P1-P2-Groenbos;Persist Security Info=True;User ID=BIM022020;Password=BiM@IH2020";
+			SqlConnection scon = new SqlConnection(mycon);
+			SqlCommand cmd = new SqlCommand();
+			SqlDataAdapter da = new SqlDataAdapter();
 		}
 
 		protected void btnBetalen_Click(object sender, EventArgs e)
 		{
-
+			// Verbinding met de database voor een pin betaling tussen de facatures
+			String mycon = "Data Source=SQL.BIM.OSOX.NL;Initial Catalog=2020-BIM02-P1-P2-Groenbos;Persist Security Info=True;User ID=BIM022020;Password=BiM@IH2020";
+			SqlConnection scon = new SqlConnection(mycon);
+			SqlCommand cmd = new SqlCommand();
+			SqlDataAdapter da = new SqlDataAdapter();
 		}
 
 		protected void btnContant_Click(object sender, EventArgs e)
 		{
-
+			// Verbininding voor een contante betaling tussen de facatures
+			String mycon = "Data Source=SQL.BIM.OSOX.NL;Initial Catalog=2020-BIM02-P1-P2-Groenbos;Persist Security Info=True;User ID=BIM022020;Password=BiM@IH2020";
+			SqlConnection scon = new SqlConnection(mycon);
+			SqlCommand cmd = new SqlCommand();
+			SqlDataAdapter da = new SqlDataAdapter();
 		}
 	}
 }
