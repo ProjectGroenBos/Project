2c07fff6b790a1ae78617d42e0217fdc10030aa2
kevinklkl <64142779+kevinklkl@users.noreply.github.com>
2020-12-03 11:55:34 +0100

    Revert "psu"
    This reverts commit 15c488162a888b7c4280cc8b8d55da5326522522.

--- ProjectGroenBos/Financien/FactuurKoppelScherm.aspx.cs
+++ ProjectGroenBos/Financien/FactuurKoppelScherm.aspx.cs
@@ -19,8 +19,6 @@ namespace ProjectGroenBos.Financien
             if (!IsPostBack)
             {
                 Repeater();
-                ScriptManager.RegisterStartupScript(this, GetType(), "Text", "getChart('ma, di', '100, 200');", true);
-
             }
         }
 
