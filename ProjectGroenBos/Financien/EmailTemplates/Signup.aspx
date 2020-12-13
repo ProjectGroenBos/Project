932783624034dc2b46c22db258e3a84c49780466
kevinklkl <64142779+kevinklkl@users.noreply.github.com>
2020-12-13 13:40:58 +0100

    idk

---
+++ ProjectGroenBos/Financien/EmailTemplates/Signup.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Signup.aspx.cs" Inherits="ProjectGroenBos.Financien.EmailTemplates.Signup" %>
+  
+<!DOCTYPE html>  
+  
+<html xmlns="http://www.w3.org/1999/xhtml">  
+<head runat="server">  
+    <title></title>  
+    <style type="text/css">  
+        .auto-style1 {  
+            height: 30px;  
+        }  
+    </style>  
+</head>  
+<body>  
+    <form id="form1" runat="server">  
+        <div>  
+            <h3>New User (Signup)</h3>  
+            <table>  
+                <tr>  
+                    <td>User Name<br />  
+                        (Email ID)  
+                    </td>  
+                    <td>  
+                        <asp:TextBox ID="txtUserName" runat="server" Width="200px"></asp:TextBox>  
+                    </td>  
+                </tr>  
+                <tr>  
+                    <td>  
+                        <br />  
+                        Password  
+                    </td>  
+  
+                    <td>  
+                        <br />  
+                        <asp:TextBox ID="txtPassword" runat="server" TextMode="Password" Width="200px"> </asp:TextBox>  
+                    </td>  
+                </tr>  
+                <tr>  
+                    <td colspan="2" class="auto-style1">  
+                        <asp:Button ID="btnSubmit" runat="server" Text="Register (Signup)" OnClick="btnSubmit_Click" />  
+                    </td>  
+                </tr>  
+            </table>  
+        </div>  
+    </form>  
+</body>  
+</html> 
---
+++ ProjectGroenBos/Financien/EmailTemplates/Signup.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjectGroenBos.Financien.EmailTemplates
+{
+    public partial class Signup : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //Fetching Settings from WEB.CONFIG file.  
+            string emailSender = "groenbosfinances@hotmail.com";
+            string emailSenderPassword = "MarionenAndries";
+            string emailSenderHost = "smtp.gmail.com";
+            int emailSenderPort = 587;
+            Boolean emailIsSSL = true;
+
+
+            //Fetching Email Body Text from EmailTemplate File.  
+            string FilePath = "C:\\Users\\telef\\Documents\\GitHub\\Project\\ProjectGroenBos\\Financien\\EmailTemplates\\SignUp.html";
+            StreamReader str = new StreamReader(FilePath);
+            string MailText = str.ReadToEnd();
+            str.Close();
+
+            //Repalce [newusername] = signup user name   
+            MailText = MailText.Replace("[newusername]", txtUserName.Text.Trim());
+
+
+            string subject = "Welcome to CSharpCorner.Com";
+
+            //Base class for sending email  
+            MailMessage _mailmsg = new MailMessage();
+
+            //Make TRUE because our body text is html  
+            _mailmsg.IsBodyHtml = true;
+
+            //Set From Email ID  
+            _mailmsg.From = new MailAddress(emailSender);
+
+            //Set To Email ID  
+            _mailmsg.To.Add(txtUserName.Text.ToString());
+
+            //Set Subject  
+            _mailmsg.Subject = subject;
+
+            //Set Body Text of Email   
+            _mailmsg.Body = MailText;
+
+
+            //Now set your SMTP   
+            SmtpClient _smtp = new SmtpClient();
+
+            //Set HOST server SMTP detail  
+            _smtp.Host = emailSenderHost;
+
+            //Set PORT number of SMTP  
+            _smtp.Port = emailSenderPort;
+
+            //Set SSL --> True / False  
+            _smtp.EnableSsl = emailIsSSL;
+
+            //Set Sender UserEmailID, Password  
+            NetworkCredential _network = new NetworkCredential(emailSender, emailSenderPassword);
+            _smtp.Credentials = _network;
+
+            //Send Method will send your MailMessage create above.  
+            _smtp.Send(_mailmsg);
+
+
+        }
+    }
+}
