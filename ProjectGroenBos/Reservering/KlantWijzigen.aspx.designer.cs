9f52c6016fc23875a93a5c675c2d4469a5cd336b
AbdelAmz99 <a.amzour99@hotmail.com>
2020-12-15 22:42:12 +0100

    klantwijzigen part1

--- ProjectGroenBos/Reservering/KlantWijzigen.aspx.designer.cs
+++ ProjectGroenBos/Reservering/KlantWijzigen.aspx.designer.cs
@@ -238,5 +238,23 @@ namespace ProjectGroenBos.Reservering
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnWijzigen;
+
+        /// <summary>
+        /// GridView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
+
+        /// <summary>
+        /// SqlDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
     }
 }
