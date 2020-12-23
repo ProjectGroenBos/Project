6e5894cb390babcd583ed5d69cd24ebf05930793
Ambergekleurd <71022538+Ambergekleurd@users.noreply.github.com>
2020-12-23 02:23:46 +0100

    Leverancier inzien

--- ProjectGroenBos/Financien/Leverancier.aspx.designer.cs
+++ ProjectGroenBos/Financien/Leverancier.aspx.designer.cs
@@ -11,26 +11,26 @@ namespace ProjectGroenBos.Financien
 {
 
 
-    public partial class WebForm3
+    public partial class Leverancier
     {
 
         /// <summary>
-        /// gvPersoneelsOverzicht control.
+        /// gvLeveranciersOverzicht control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView gvPersoneelsOverzicht;
+        protected global::System.Web.UI.WebControls.GridView gvLeveranciersOverzicht;
 
         /// <summary>
-        /// SqlDataSource4 control.
+        /// SqlDataSource1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource4;
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
 
         /// <summary>
         /// txbNaam control.
